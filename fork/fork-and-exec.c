a3924ff68c639f19492e34bf1093a9e398558c4f
daikw <hazuhi@gmail.com>
2019-01-01 11:52:52 +0000

    :point_right: try to implement the fork-and-exec pattern

---
+++ fork/fork-and-exec.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+static void chiled()
+{
+	char *args[] = { "/bin/echo", "hello", NULL};
+	printf("I'm child! my pid is %d.\n",getpid());
+	fflush(stdout);
+	execve("/bin/echo", args, NULL);
+	err(EXIT_FAILURE, "exec() failed");
+}
+
+static void parent(pid_t pid_c)
+{
+	printf("I'm parent! my pid is %d and the pid of my child is %d.\n", getpid(), pid_c);
+	exit(EXIT_SUCCESS);
+}
+
+int main(void)
+{
+	pid_t ret;
+	ret = fork();
+	if (ret == -1)
+		err(EXIT_FAILURE, "fork() failed");
+	if (ret == 0) {
+		chiled();
+	} else {
+		parent(ret);
+	}
+	err(EXIT_FAILURE, "shouldn't reach here");
+}
+
